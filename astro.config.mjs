e308f9583dacfe5ed92cd5928b9cd542b6e4bcd0
observerw <wozluohd@gmail.com>
2026-01-13 19:03:23 +0800

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,18 @@
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
+import rehypeKatex from "rehype-katex";
 import remarkGfm from "remark-gfm";
+import remarkMath from "remark-math";
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [react()],
 	markdown: {
-		remarkPlugins: [remarkGfm],
+		remarkPlugins: [remarkGfm, remarkMath],
+		rehypePlugins: [[rehypeKatex, {
+			output: 'html'
+		}]],
 	},
 	vite: {
 		plugins: [tailwindcss()],
